23452037fdfc785f49c5d8c71bd14b2e1ba85c78
shweta_garg <shwetagarg2006@gmail.com>
2014-09-22 05:23:08 +0530

    Added shell script

---
+++ preprocess.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ham=1
+while read -a line
+do
+    label=${line[0]}
+    filename=${line[1]}
+    if [ "$label" == "$ham" ]
+    then
+    	cp $filename "ham/$filename"
+    else
+    	cp $filename "spam/$filename"
+    fi   
+done
+
+
+
