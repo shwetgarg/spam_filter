f0c9ba3ead480d9ec45c44ad57c02d25a127f1ad
shweta_garg <shwetagarg2006@gmail.com>
2014-09-22 05:27:20 +0530

    Small change

--- utils.py
+++ utils.py
@@ -15,23 +15,23 @@ def get_kfold_data(spam_set, ham_set, num_folds):
 		test_ham_set = ham_set[i*ham_chunk_size:(i+1)*ham_chunk_size]
 		train_ham_set = ham_set[:i*ham_chunk_size] + ham_set[(i+1)*ham_chunk_size:]
 		yield (train_spam_set + train_ham_set, test_spam_set, test_ham_set)
-	
+
 def get_file_data(path):
 	'''
 	Read content of a file and return
 	'''    
 	with open(path, 'rb') as f:
 		data = f.read()
-	return data        
+		return data        
 
 def get_dir_data(path):
-	'''
-	Read and return content of all files in a directory
-	'''    
-	filelist = os.listdir(path)
-	data = [get_file_data(os.path.join(path, f)) for f in filelist]
-	return data
-    
+    '''
+    Read and return content of all files in a directory
+    '''    
+    filelist = os.listdir(path)
+    data = [get_file_data(os.path.join(path, f)) for f in filelist]
+    return data
+
 def get_dir_data_with_filename(path):
     '''
     Read and return content of all files in a directory with filename
@@ -39,12 +39,12 @@ def get_dir_data_with_filename(path):
     filelist = os.listdir(path)
     data = [(get_file_data(os.path.join(path, f)), f) for f in filelist]
     return data
-    
+
 def write_file(filename, iterator):
-	'''
-	Write content of iterator in given file
-	'''
-	with open(filename, 'wb') as f:
-		for line in iterator:
-			f.write(line)
-    		
+    '''
+    Write content of iterator in given file
+    '''
+    with open(filename, 'wb') as f:
+        for line in iterator:
+            f.write(line)
+
